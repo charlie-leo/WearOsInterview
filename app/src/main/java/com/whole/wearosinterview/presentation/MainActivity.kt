2ac8b9b08d64c23a979e15159148b37c649ff845
Charles Raj <charles.raj412@gmail.com>
2023-10-05 00:22:10 +0530

    Small changes

--- app/src/main/java/com/whole/wearosinterview/presentation/MainActivity.kt
+++ app/src/main/java/com/whole/wearosinterview/presentation/MainActivity.kt
@@ -12,7 +12,6 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,9 +19,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -32,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -56,7 +56,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginForm(errorMessage: String, onSubmit: (String, String) -> Unit) {
+fun LoginForm(errorMessage: String, onSubmit: (String, String,Int) -> Unit) {
     var email by rememberSaveable {
         mutableStateOf("")
     }
@@ -80,7 +80,7 @@ fun LoginForm(errorMessage: String, onSubmit: (String, String) -> Unit) {
 
 
             Text(
-                text = "Username", // Your hint text
+                text = "Username",
                 color = Color.White,
                 modifier = Modifier
                     .padding(4.dp, 1.dp, 1.dp, 1.dp)
@@ -94,8 +94,6 @@ fun LoginForm(errorMessage: String, onSubmit: (String, String) -> Unit) {
                     if (!TextUtils.isEmpty(newEmail)) {
                         email = newEmail
                     }
-//                    email = email.plus(newEmail)
-                    Log.d("TAG", "LoginForm: $email")
                 },
                 textStyle = TextStyle(fontSize = 10.sp, color = Color.White),
                 modifier = Modifier
@@ -115,7 +113,7 @@ fun LoginForm(errorMessage: String, onSubmit: (String, String) -> Unit) {
             )
 
             Text(
-                text = "Password", // Your hint text
+                text = "Password",
                 color = Color.White,
                 modifier = Modifier
                     .padding(4.dp, 1.dp, 1.dp, 1.dp)
@@ -161,7 +159,7 @@ fun LoginForm(errorMessage: String, onSubmit: (String, String) -> Unit) {
 
             Button(
                 onClick = {
-                    onSubmit(email, password)
+                    onSubmit(email, password,0)
                 },
                 modifier = Modifier
                     .width(80.dp)
@@ -177,15 +175,37 @@ fun LoginForm(errorMessage: String, onSubmit: (String, String) -> Unit) {
                 )
             }
 
-            Text(
-                text = "Don't have an account Sign Up.", // Your hint text
-                color = Color.White,
+            ClickableText(
+                text = AnnotatedString("Don't have an account Sign Up."),
                 modifier = Modifier
                     .padding(4.dp)
                     .fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                fontSize = 8.sp
-            )
+                style = TextStyle(color = Color.White,
+                    textAlign = TextAlign.Center,
+                    fontSize = 8.sp),
+                    onClick = {
+                        onSubmit(email, password,1)
+                    })
+
+//            Column (
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .clickable {
+//                    },
+//                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            ) {
+//                Text(
+//                    text = "Don't have an account Sign Up.", // Your hint text
+//                    color = Color.White,
+//                    modifier = Modifier
+//                        .padding(4.dp)
+//                        .fillMaxWidth(),
+//                    textAlign = TextAlign.Center,
+//                    fontSize = 8.sp
+//                )
+//            }
+
 
         }
 
@@ -198,20 +218,45 @@ fun LoginForm(errorMessage: String, onSubmit: (String, String) -> Unit) {
 fun LoginScreen() {
     var loginSuccessful by remember { mutableStateOf(false) }
     var errorMessage by remember { mutableStateOf("") }
-
-    if (loginSuccessful) {
-        DashboardScreen()
-    } else {
-        LoginForm(errorMessage) { username, password ->
+    var signUpClick by remember {
+        mutableStateOf(false)
+    }
+    var signUpResponse by remember {
+        mutableStateOf(false)
+    }
+    if (signUpClick){
+        SignUpScreen(errorMessage){ username, password, type ->
             if (!TextUtils.isEmpty(username)
                 && !TextUtils.isEmpty(username)
             ) {
-                loginSuccessful = true
+                signUpResponse = true
+                signUpClick = false
             } else {
-                errorMessage = "Invalid username or password"
+                errorMessage = "Email and Password can't be empty"
             }
-
         }
+        return
+    }
+
+    if (loginSuccessful) {
+        DashboardScreen()
+        return
+
+    } else {
+        LoginForm(errorMessage) { username, password, type ->
+            if (type == 0) {
+                if (!TextUtils.isEmpty(username)
+                    && !TextUtils.isEmpty(username)
+                ) {
+                    loginSuccessful = true
+                } else {
+                    errorMessage = "Invalid username or password"
+                }
+            } else{
+                signUpClick = true
+            }
+        }
+
     }
 }
 
@@ -223,8 +268,140 @@ fun DashboardScreen() {
         modifier = Modifier.fillMaxWidth(),
         textAlign = TextAlign.Center
     )
+}
+
+@Composable
+fun SignUpScreen(errorMessage: String,onSubmit: (String, String,Int) -> Unit){
+
+    var email by rememberSaveable {
+        mutableStateOf("")
+    }
+    var password by rememberSaveable { mutableStateOf("") }
 
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "SignUp",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontSize = 13.sp,
+        )
+
+
+        Text(
+            text = "Username", // Your hint text
+            color = Color.White,
+            modifier = Modifier
+                .padding(4.dp, 1.dp, 1.dp, 1.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Left,
+            fontSize = 10.sp,
+        )
+        BasicTextField(
+            value = email,
+            onValueChange = { newEmail ->
+                if (!TextUtils.isEmpty(newEmail)) {
+                    email = newEmail
+                }
+            },
+            textStyle = TextStyle(fontSize = 10.sp, color = Color.White),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .border(
+                    width = 1.dp,
+                    color = Color.White,
+                    shape = RoundedCornerShape(3.dp)
+                )
+                .padding(4.dp),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Done
+            ),
+        )
+
+        Text(
+            text = "Password",
+            color = Color.White,
+            modifier = Modifier
+                .padding(4.dp, 1.dp, 1.dp, 1.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Left,
+            fontSize = 10.sp,
+        )
+        BasicTextField(
+            value = password,
+            onValueChange = {
+                if (!TextUtils.isEmpty(it)) {
+                    password = it
+                }
+            },
+            textStyle = TextStyle(fontSize = 10.sp, color = Color.White),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .border(
+                    width = 1.dp,
+                    color = Color.White,
+                    shape = RoundedCornerShape(3.dp)
+                )
+                .padding(4.dp),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            visualTransformation = PasswordVisualTransformation()
+
+        )
+
+        if (!TextUtils.isEmpty(errorMessage)) {
+            Text(
+                text = errorMessage,
+                fontSize = 10.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Left,
+                color = Color.Red
+            )
+        }
+
+        Button(
+            onClick = {
+                onSubmit(email, password,0)
+            },
+            modifier = Modifier
+                .width(80.dp)
+                .height(20.dp)
+                .padding(1.dp)
+
+
+        ) {
+            Text(
+                text = stringResource(id = R.string.signup),
+                color = Color.White,
+                fontSize = 10.sp
+            )
+        }
+
+        ClickableText(
+            text = AnnotatedString("Already have an account?"),
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            style = TextStyle(color = Color.White,
+                textAlign = TextAlign.Center,
+                fontSize = 8.sp),
+            onClick = {
+                onSubmit(email, password,1)
+            })
+    }
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
